1b7d9df88b3c9b0bba41a60ae6595fd53e43f517
KA1ANDA <tokokalanda20@gmail.com>
2023-07-06 21:09:24 +0400

    change

--- src/index.js
+++ src/index.js
@@ -4,6 +4,21 @@ import App from './Components/AppUi/App';
 import { Provider } from 'react-redux';
 import { store } from './Redux/store';
 
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors({
+  origin: 'https://ka1anda.github.io'
+}));
+
+// Add your routes and other middleware here
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
